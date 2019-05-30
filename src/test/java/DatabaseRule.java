894d66f60ff004a4f1c7340a448a76f07b91fb68
developer <emakuthi@gmail.com>
2019-05-30 14:45:08 +0300

    create databasearule class and two main classes engineer and site

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,12 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.Sql2o;
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/site_maintenance", elvis, elvis);  //Those with linux or windows use two strings for username and password
+    }
+
+
+}
