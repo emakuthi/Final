2541be7c47bfca5a5f7221ed048c16ee365a15cf
Nashipae <nashkiki@gmail.com>
2019-07-14 09:42:02 +0300

    added the asset class and interface.

---
+++ src/main/java/models/Asset.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Asset {
+    private int id;
+    private String equipmentSerialNumber;
+    private String equipmentName;
+    private String personWithEquipment;
+    private String moverId;
+    private String typeOfMovement;
+    private String phonenumber;
+    private String destination;
+    private String verifier;
+
+}
