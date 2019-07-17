95dcdecbb5d77786baaa3364fdd4f696a82b4d0c
developer <emakuthi@gmail.com>
2019-07-17 18:21:42 +0300

    add checkout

---
+++ src/test/java/dao/VisitorDaoTest.java
@@ -0,0 +1,23 @@
+package dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VisitorDaoTest {
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void findById() {
+    }
+    @Test
+    public void update() {
+    }
+}
